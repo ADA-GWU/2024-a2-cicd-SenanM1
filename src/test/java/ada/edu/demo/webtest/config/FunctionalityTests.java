a65b0482408c0c784323565b9348c0dcd2f31cd9
SenanM1 <senan.mardanli@gmail.com>
2024-03-11 18:27:26 +0400

    3 Test cases added

---
+++ src/test/java/ada/edu/demo/webtest/config/FunctionalityTests.java
@@ -0,0 +1,70 @@
+package ada.edu.demo.webtest.config;
+
+import ada.edu.demo.webtest.entity.Student;
+import ada.edu.demo.webtest.repository.StudentRepository;
+import ada.edu.demo.webtest.service.StudentService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class FunctionalityTests {
+
+    @Mock
+    private StudentRepository studentRepository;
+
+    @InjectMocks
+    private StudentService studentService;
+
+    @Test
+    @DisplayName("Test finding s student by ID")
+    public void testStudentSearchById() {
+        when(studentRepository.findById(1)).thenReturn(Optional.of(new Student()));
+        Student result = studentService.getStudentById(1);
+        assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("Search by first or last name")
+    public void testStudentSearch() {
+        Student s1 = new Student(1,"Jamal","Hasanov","a@b.com",new Date(),null,null);
+        Student s2 = new Student(2,"Aliya","Mammadova","a@b.com",new Date(),null,null);
+        Student s3 = new Student(3,"Kamran","Aliyev","a@b.com",new Date(),null,null);
+
+        List<Student> stList = List.of(s1,s2,s3);
+
+        when(studentRepository.findAll()).thenReturn(stList);
+        List<Student> students = studentService.getStudentByEitherName("Jamal","Aliyev");
+        System.out.printf("Found students: "+students.size());
+        assertEquals(2, students.size() );
+    }
+
+    @Test
+    @DisplayName("Test updating a student")
+    public void testStudentUpdate() {
+        Student original = new Student(1, "Snan", "Mrdanli", "smardanli@email.com", new Date(), null, null);
+        Student updated = new Student(1, "Sanan", "Mardanli", "senanmardanli@hotmail.com", new Date(), null, null);
+
+        lenient().when(studentRepository.findById(1)).thenReturn(Optional.of(original));
+        lenient().when(studentRepository.save(updated)).thenReturn(updated); // Ensure save behavior is mocked
+
+        Student result = studentService.saveStudent(updated);
+        assertNotNull(result);
+        assertEquals("Sanan", result.getFirstName());
+        assertEquals("Mardanli", result.getLastName());
+        assertEquals("senanmardanli@hotmail.com", result.getEmail());
+    }
+
+
+}
